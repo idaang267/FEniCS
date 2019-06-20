a3223001cdef2b13d0735775d9c672eb2faf1bce
idaang267 <51921772+idaang267@users.noreply.github.com>
2019-06-20 14:56:49 -0400

    Code is running
    Code is running but each time step of the solution are not saving accurately.

--- Swelling.py
+++ Swelling.py
@@ -5,6 +5,7 @@
 
 # Import dolfin module
 from dolfin import *
+import matplotlib.pyplot as plt     # Module matplotlib
 
 # Solver parameters: Using PETSc SNES solver
 snes_solver_parameters = {"nonlinear_solver": "snes",
@@ -35,6 +36,12 @@ class InitialConditions(UserExpression):
     def value_shape(self):
          return (4,)
 
+class StepConc(UserExpression):
+    def eval(self, values, x):
+        values[0] = J - 1
+    def value_shape(self):
+        return (1,)
+
 # Class for interfacing with the Newton solver
 class Equation(NonlinearProblem):
     # Bilinear and linear forms are used to compute the residual and Jacobian
@@ -56,8 +63,8 @@ class Equation(NonlinearProblem):
 #------------------------------------------------------------------------------
 B  = Constant((0.0, 0.0, 0.0))  # Body force per unit volume
 T  = Constant((0.0, 0.0, 0.0))  # Traction force on the boundary
-chi = 0.4                       # Flory Parameter
-l0 = 1.1                        # Initial Stretch (lambda_o)
+chi = 0.6                       # Flory Parameter
+l0 = 1.55                       # Initial Stretch (lambda_o)
 # Normalization Parameters
 bulkMod = 10**3                 # Bulk Modulus (K/N k_B T)
 n = 10**(-3)                    # Normalization (N Omega)
@@ -92,10 +99,10 @@ front = CompiledSubDomain("near(x[2], side) && on_boundary", side = 1.0)
 
 # Bottom of the cube is attached to substrate and has fixed displacement from
 # the reference relative to the current configuration
-u_bot = Expression(("(1-scale)*x[0]", "0.0*x[1]", "(1-scale)*x[2]"), scale=l0, degree=1)
+u_bot = Expression(("(scale-1)*x[0]", "0.0*x[1]", "(scale-1)*x[2]"), scale=l0, degree=1)
     # Account for initial configuration: displacement in x & z direction not y
 # Chemical potential BC equivalent to mu0 in the IC class
-chem_pot = (1/l0**3)*(l0/bulkMod-1/bulkMod+1) + ln((l0**3-1)/l0**3) + chi/l0**6
+chem_pot =0#(1/l0**3)*(l0/bulkMod-1/bulkMod+1) + ln((l0**3-1)/l0**3) + chi/l0**6
 
 # The Dirichlet BCs are specified in respective subspaces
 # Displacement in first subspace V.sub(0)
@@ -117,7 +124,7 @@ w0 = Function(V)                            # Previous solution for u0 and mu0
 # Split test functions and unknowns (produces a shallow copy not a deep copy)
 (v_u, v_mu) = split(v)
 (u, mu) = split(w)
-(u0, mu0) = split(w)
+(u0, mu0) = split(w0)
     # Deep copy syntax: w.split(deepcopy=True)
 
 # Initial Conditions (IC)
@@ -141,47 +148,50 @@ Ic = tr(CG)
 J = det(F)                      # Current time step
 J_prev = det(F_prev)            # Previous time step
 
-# Definition for normalized nominal stress tensor: P = dU/dF
+# Definitions
+#------------------------------------------------------------------------------
+# Normalized nominal stress tensor: P = dU/dF
 def P(u, mu):
     return F + (-1/J + (1/n)*(1/J + ln((J-1)/J) + chi/(J**2) - mu))*J*inv(F.T)
 
 # Definition of concentration where C = - dU/dmu = (J-1)
-def C_prev(u0):
-    return J_prev-1
-def C(u):
-    return J-1
+C_prev = J_prev-1
+C = J-1
 
 # Definition for normalized flux (Don't use J)
 def Flux(u, mu):
     p1 = dot(inv(F), grad(mu))
-    return -C(u)*dot(inv(F),p1)
+    return -C*dot(inv(F),p1)
 
 # Variational problem where we have two equations for the weak form
 F0 = inner(P(u, mu), grad(v_u))*dx - inner(T, v_u)*ds - dot(B, v_u)*dx
-F1 = (1/n)*(inner(C(u), v_mu)*dx - inner(C_prev(u0), v_mu)*dx - inner(Flux(u, mu), grad(v_mu))*dt*dx)
+F1 = (1/n)*(inner(C, v_mu)*dx - inner(C_prev, v_mu)*dx - inner(Flux(u, mu), grad(v_mu))*dt*dx)
 WF = F0 + F1
 
 # Compute directional derivative about w in the direction of du (Jacobian)
 J = derivative(WF, w, du)
 
+'''
 # Create nonlinear problem and Newton solver
 problem = Equation(J, WF, bc)
 solver = NewtonSolver()
 solver.parameters["linear_solver"] = "lu"
 solver.parameters["convergence_criterion"] = "incremental"
 solver.parameters["relative_tolerance"] = 1e-6
+'''
 
-# Use if trying SNES
-"""
+# Use for SNES solver
 # Setup Non-linear variational problem
 problem = NonlinearVariationalProblem(WF, w, bc, J=J)
 solver_problem = NonlinearVariationalSolver(problem)
 solver_problem.parameters.update(snes_solver_parameters)
-"""
 
 # Save results to an .xdmf file since we have multiple fields (time-dependence)
 file_results = XDMFFile("results.xdmf")
 
+# Define concentrations as functions in order to update
+C_prev = Function(V)
+C = Function(V)
 # Solve for each value using the previous solution as a starting point
 while (t < Time):
 
@@ -192,11 +202,15 @@ while (t < Time):
     w0.vector()[:] = w.vector()
 
     # Solve
-    #solver_problem.solve()
-    solver.solve(problem, w.vector())
+    solver_problem.solve()
+    #solver.solve(problem, w.vector())
 
     # Note that this is now a deep copy not a shallow copy like split(w)
     (u, mu) = w.split()               # Split the test functions
+
+    # Update previous solution for Concentration
+    C_prev.assign(C)
+        # Assign computed C to C_prev (previous)
 
     # Write results to file_results defined
     u.rename("Displacement", "u")
@@ -204,6 +218,7 @@ while (t < Time):
     # Parameters will share the same mesh
     file_results.parameters["flush_output"] = True
     file_results.parameters["functions_share_mesh"] = True
+
     # Write to .xdmf results file
     file_results.write(u,t)
     file_results.write(mu,t)
