05fd3b8d8eba399eeadd002bad9bae91e1fee0e8
idaang267 <51921772+idaang267@users.noreply.github.com>
2019-07-03 09:55:53 -0400

    Updated time stepping
    Solution was not updating before. Currently edited line 249 instead of using c_steps. Next step is to figure out an exponential dt.

--- Swelling.py
+++ Swelling.py
@@ -42,8 +42,8 @@ class TopConstraint(SubDomain):
     # Top boundary is "target domain" G without one point at [1, 1, 1]
     def inside(self, x, on_boundary):
         return bool((x[1] > 1.0 - DOLFIN_EPS and x[0] < 1.0 - DOLFIN_EPS) or \
-                    (x[1] > 1.0 - DOLFIN_EPS and x[2] < 1.0 - DOLFIN_EPS) and
-                    on_boundary)
+                    (x[1] > 1.0 - DOLFIN_EPS and x[2] < 1.0 - DOLFIN_EPS) )#and  on_boundary)
+
     # Map a coordinate x in top boundary to a coordinate y in target boundary (G)
     def map(self, x, y):
         # Map to one point at [1, 1, 1]
@@ -70,23 +70,22 @@ class Equation(NonlinearProblem):
 
 # Model parameters
 #------------------------------------------------------------------------------
-name = "Result.xdmf"    # Name of file
+name = "Result_chi0.4_l01.4.xdmf"    # Name of file
 B  = Constant((0.0, 0.0, 0.0))  # Body force per unit volume
 T  = Constant((0.0, 0.0, 0.0))  # Traction force on the boundary
-chi = 0.5                       # Flory Parameter
-l0 = 1.4                        # Initial Stretch (lambda_o)
+chi = 0.4 #0.6                       # Flory Parameter
+l0 = 1.4 #1.3                        # Initial Stretch (lambda_o)
 # Normalization Parameters
 bulkMod = 10**3                 # Bulk Modulus (K/N k_B T)
 n = 10**(-3)                    # Normalization (N Omega)
 # Time and Stepping Parameters
 #------------------------------------------------------------------------------
-t = 0.0                         # Initial time (updated within loop)
 steps = 0                       # Steps (updated within loop)
 c_steps = 0                     # Chemical step counter (updated within loop)
 t_c_steps = 6                   # Total chemical steps
-tot_steps = 8                   # Total number of time steps
+tot_steps = 100                 # Total number of time steps
 # Set up starting and ending time step for exponential fit
-dt_first = 10**(-6)             # Beginning point
+dt_first = 10**(-1)             # Beginning point
 dt_last = 10**(-1)              # End point
 
 test_x = [0, tot_steps]
@@ -100,8 +99,9 @@ a = popt[0]                     # Fitting parameter 1
 b = popt[1]                     # Fitting parameter 2
 x = np.linspace(0, tot_steps, tot_steps)
 dt = a*x + b                    # Change in time between time steps
-#print(dt)
+print(dt)
 
+#DT = Expression("dt", dt=0, degree=0)
 '''
 plt.plot(test_x, test_y, 'b--')
 plt.plot(x, dt, 'g--', label='fit: a=%5.3f b=%5.3f' % tuple(popt))
@@ -118,7 +118,7 @@ TT = TensorFunctionSpace(mesh,'DG',0)               # For projection of stress
 # Set periodic boundary condition with a tolerance value
 pbc = TopConstraint(1E-5)
 # Define Taylor-Hood Elements
-P2 = VectorFunctionSpace(mesh, "Lagrange", 2, constrained_domain=pbc)
+P2 = VectorFunctionSpace(mesh, "Lagrange", 2) #, constrained_domain=pbc)
     # Second order quadratic interpolation for displacement (u)
 P1 = FunctionSpace(mesh, "Lagrange", 1)
     # First order linear interpolation for chemical potential
@@ -127,13 +127,6 @@ P2elem = P2.ufl_element()
 P1elem = P1.ufl_element()
 # Define mixed function space specifying underying finite element
 V = FunctionSpace(mesh, P2elem * P1elem)
-
-# P2 = VectorElement("Lagrange", mesh.ufl_cell(), 2)  # Displacement (u)
-#     # Second order quadratic interpolation for displacement
-# P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)  # Chemical Potential (mu)
-#     # First order linear interpolation for chemical potential
-# TH = MixedElement([P2, P1])
-# V  = FunctionSpace(mesh, TH, constrained_domain=pbc)
 
 # Define functions in mixed function space V
 #------------------------------------------------------------------------------
@@ -185,12 +178,8 @@ bc_r_r = DirichletBC(V.sub(0).sub(0), u_lr, right)
 bc_r_b = DirichletBC(V.sub(0).sub(2), u_bf, back)
 bc_r_f = DirichletBC(V.sub(0).sub(2), u_bf, front)
 
-# Chemical potential in second subspace V.sub(1) on exposed surfaces
+# Chemical potential in second subspace V.sub(1) on top surfaces
 bc_t = DirichletBC(V.sub(1), chem_p, top)
-bc_l = DirichletBC(V.sub(1), chem_p, left)
-bc_r = DirichletBC(V.sub(1), chem_p, right)
-bc_b = DirichletBC(V.sub(1), chem_p, back)
-bc_f = DirichletBC(V.sub(1), chem_p, front)
 
 # Combined boundary conditions
 bc = [bc_u, bc_r_l, bc_r_r, bc_r_b, bc_r_f, bc_t]
@@ -244,17 +233,22 @@ solver_problem.parameters.update(snes_solver_parameters)
 # Save results to an .xdmf file since we have multiple fields (time-dependence)
 file_results = XDMFFile(name)
 
+t = 0.0                         # Initial time (updated within loop)
 # Solve for each value using the previous solution as a starting point
 while (steps < tot_steps):
-    steps += 1                      # Update total steps
     # Update the chemical steps for ramping of chemical potential
     if c_steps < t_c_steps:
         c_steps += 1
     c_steps += 0
     chem_p.c_steps = c_steps        # Update steps in expression class
+    print(c_steps)
+
     w0.vector()[:] = w.vector()     # Update fields containing u and mu
-    t += dt[c_steps-1]              # Update time
     solver_problem.solve()          # Solve using the parameters setup
+
+    t += dt[steps-1]                # Update time
+    steps += 1                      # Update total steps
+    print(steps)
 
     # Note that this is now a deep copy not a shallow copy like split(w)
     # allowing us to write the results to file
