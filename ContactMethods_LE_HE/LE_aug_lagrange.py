ea7a450826d5d01e1175aa00301fa88a520cf640
Ida Ang <ia267@cornell.edu>
2019-10-16 09:53:38 -0400

    Final version with comments
    Commented carefully. Final version

--- ContactMethods_LE_HE/LE_aug_lagrange.py
+++ ContactMethods_LE_HE/LE_aug_lagrange.py
@@ -4,8 +4,8 @@
 from dolfin import *
 from multiphenics import *
 from mshr import *
-from ufl import rank
 
+# PETSc SNES solver: non-linear solver parameters
 snes_solver_parameters = {"nonlinear_solver": "snes",
                           "snes_solver": {"linear_solver": "mumps",
                                           "maximum_iterations": 20,
@@ -13,10 +13,11 @@ snes_solver_parameters = {"nonlinear_solver": "snes",
                                           "error_on_nonconvergence": False}}
 
 # Parameters
-name = "LE_Aug_Lagrange.xdmf"
-# Steps
-steps = 0
-tot_steps = 20
+# -----------------------------------------------------------------------------
+name = "LE_Aug_Lagrange.xdmf"       # File name
+# Stepping parameters
+steps = 0                           # Updates in the loop
+tot_steps = 20                      # Total amount of steps
 # Define the indenter (obstacle) with a parabola
 R = 0.25                            # Radius
 depth = 0.00                        # Depth
@@ -25,12 +26,33 @@ E = Constant(10.0)                  # Young's Modulus
 nu = Constant(0.3)                  # Poisson's Ratio
 mu = E/(2*(1+nu))                   # Lamé Coefficient
 lmbda = E*nu/((1+nu)*(1-2*nu))      # Lamé Coefficient
-# Initial Stretch (lambda_o)
-l0 = 1.4
 # Augmented lagrangian constant: epsilon_N
 k_pen = 1E4
 
-# Create Mesh
+# Define class
+# -----------------------------------------------------------------------------
+class top(SubDomain):           # Top surface
+    def inside(self, x, on_boundary):
+        return near(x[1], 1.0) and on_boundary
+class bot(SubDomain):           # Bottom surface
+    def inside(self, x, on_boundary):
+        return near(x[1], 0.0) and on_boundary
+# Lateral surfaces
+class symmetry_x(SubDomain):    # left and right surfaces
+    def inside(self, x, on_boundary):
+        return near(x[0], 0.0) or near(x[0], 1.0) and on_boundary
+class symmetry_z(SubDomain):    # front and back surfaces
+    def inside(self, x, on_boundary):
+        return near(x[2], 0.0) or near(x[2], 1.0) and on_boundary
+
+# Convert classes
+top = top()
+bot = bot()
+sym_x = symmetry_x()
+sym_z = symmetry_z()
+
+# Create Mesh and define subdomains for restrictions and boundary conditions
+# -----------------------------------------------------------------------------
 domain = Box(Point(0., 0., 0.), Point(1.0, 1.0, 1.0))
 mesh = generate_mesh(domain, 25)
 
@@ -39,29 +61,22 @@ subdomains = MeshFunction("size_t", mesh, mesh.topology().dim(), mesh.domains())
 # Create boundaries
 boundaries = MeshFunction("size_t", mesh, mesh.topology().dim() - 1)
 
-# Define class
-class top(SubDomain):
-    def inside(self, x, on_boundary):
-        return near(x[1], 1.0) and on_boundary
-
-class bot(SubDomain):
-    def inside(self, x, on_boundary):
-        return near(x[1], 0.0) and on_boundary
-
 # Mark boundary to restrict the lagrange multiplier to
-top = top()
-bot = bot()
 top.mark(boundaries, 1)
-
-# Dirichlet boundary
 boundary_restriction = MeshRestriction(mesh, top)
 
-# Function space
+# Measure
+dx = Measure("dx")(subdomain_data=subdomains)
+ds = Measure("ds")(subdomain_data=boundaries)
+
+# Define all function spaces
+# -----------------------------------------------------------------------------
 V = VectorFunctionSpace(mesh, "Lagrange", 2)
 V1 = FunctionSpace(mesh, "Lagrange", 2)
-V2 = FunctionSpace(mesh, "CG", 1) # Gap function
 # Block mixed equal order function space
 W = BlockFunctionSpace([V, V1], restrict=[None, boundary_restriction])
+# Function spaces for interpolation
+V2 = FunctionSpace(mesh, "CG", 1) # Gap function
 
 # Define trial and test functions
 du = BlockTrialFunction(W)
@@ -73,27 +88,12 @@ gap = Function(V2, name="Gap")
 (u, l) = block_split(ul)
 (v_u, v_l) = block_split(v)
 
-# Measure
-dx = Measure("dx")(subdomain_data=subdomains)
-ds = Measure("ds")(subdomain_data=boundaries)
-
 # Boundary Conditions
 # -----------------------------------------------------------------------------
 u_bot = Expression(("0.0*x[0]", "0.0*x[1]", "0.0*x[2]"), degree=1)
 # Roller boundary conditions (define normal directions) on lateral surfaces
 u_lr = Expression(("(0.0)*x[0]"), degree=1)
 u_bf = Expression(("(0.0)*x[2]"), degree=1)
-
-# Lateral surfaces
-class symmetry_x(SubDomain):    # left and right surfaces
-    def inside(self, x, on_boundary):
-        return near(x[0], 0.0) or near(x[0], 1.0) and on_boundary
-class symmetry_z(SubDomain):    # front and back surfaces
-    def inside(self, x, on_boundary):
-        return near(x[2], 0.0) or near(x[2], 1.0) and on_boundary
-
-sym_x = symmetry_x()
-sym_z = symmetry_z()
 
 # Fix bottom surface
 bc_bot = DirichletBC(W.sub(0), u_bot, bot)
@@ -127,32 +127,42 @@ indent = Expression(("-depth+(pow((x[0]-0.5),2)+pow((x[2]-0.5),2))/(2*R)"), \
 # Weak form
 F = [inner(sigma(u), eps(v_u))*dx + v_u[1]*ppos(aug_l(l))*ds,
     v_l*(u[1]-indent)*ds - (1/k_pen)*l*v_l*ds]
+
 # Jacobian
 J = block_derivative(F, ul, du)
 
-# Solve with PETsc SNES solver
+# Solve variational problem using PETSc SNES solver
 problem = BlockNonlinearProblem(F, ul, bc, J)
 solver = BlockPETScSNESSolver(problem)
 solver.parameters.update(snes_solver_parameters["snes_solver"])
 
+# Save results to an .xdmf file since we have multiple fields
 results = XDMFFile(name)
+
+# Solve with Newton solver using the previous solution as a starting point
 while steps <= tot_steps:
-    print(steps)
+    # Print outs to track code progress
+    print("Steps: " + str(steps))
+    print("Depth: " + str(depth))
+
+    # Solve the nonlinear problem
     solver.solve()
-    # Update
-    steps += 1
-    depth += 0.01
-    indent.depth = depth
+
+    # Update parameters
+    depth += 0.01               # Update the depth of the indenter
+    indent.depth = depth        # Update depth in expression class
+    steps += 1                  # Update steps
+
     # Split results
     (u, l) = ul.block_split()
-    # Save results
+    # Rename results for visualization in Paraview
     u.rename("Displacement", "u")
     l.rename("Lagrange Multiplier", "l")
     gap.assign(project(indent-u[1], V2))
     # Parameters will share the same mesh
     results.parameters["flush_output"] = True
     results.parameters["functions_share_mesh"] = True
-    # Write
+    # Write to .xdmf results file
     results.write(u, steps)
     results.write(l, steps)
     results.write(gap, steps)
