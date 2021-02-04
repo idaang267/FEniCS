daa53b28feaf0c44839ebaf646c4186d6e023e5d
Ida <ia267@cornell.edu>
2021-02-04 18:43:49 -0500

    Stress Profile Results

--- Phase_Field_Fracture/NominalStressProfile.m
+++ Phase_Field_Fracture/NominalStressProfile.m
@@ -2,10 +2,10 @@ clear all
 close all
 
 Y = 2;
-Nx = [250, 250, 250, 250, 500, 500, 500, 500, 625, 625, 625, 625, 1250, 1250, 1250, 1250];
-Ny = [100, 100, 100, 100, 200, 200, 200, 200, 100, 100, 100, 100, 100, 100, 100, 100];
-kappa = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
-ellx = [5, 10, 15, 20, 5, 10, 15, 20, 5, 10, 15, 20, 5, 10, 15, 20];
+Nx = [250, 250, 250, 250];
+Ny = [100, 100, 100, 100];
+kappa = [10, 10, 10, 10];
+ellx = [5, 10, 15, 20];
 % colors = {[0, 0, 0], [0 0.4470 0.7410],...
 %           [0, 0, 0], [0 0.4470 0.7410],...
 %           [0, 0, 0], [0 0.4470 0.7410],...
@@ -16,12 +16,12 @@ colors = {[0, 0, 0], [0 0.4470 0.7410], [0.3010 0.7450 0.9330],[0.4940 0.1840 0.
     [0, 0, 0], [0 0.4470 0.7410], [0.3010 0.7450 0.9330],[0.4940 0.1840 0.5560],...
     [0, 0, 0], [0 0.4470 0.7410], [0.3010 0.7450 0.9330],[0.4940 0.1840 0.5560]};
 
-SubPlotTitle = {"hsize = 0.02", "hsize = 0.01", "hsize = 0.008", "hsize = 0.004"};
+% SubPlotTitle = {"hsize = 0.02", "hsize = 0.01", "hsize = 0.008", "hsize = 0.004"};
 % SubPlotTitle = {"ellx = 5", "ellx = 10", "ellx = 15", "ellx = 20"};
 % SubPlotTitle = {"Width: 0.40", "Width: 0.80", "Width = 0.16", "Width = 0.32"};
 
-% DataPath = '/home/ida/Documents/GitHub/FEniCS/Phase_Field_Fracture/StressProfile/';
-DataPath = '/Users/idaang/Documents/GitHub/FEniCS/Phase_Field_Fracture/StressProfile/';
+DataPath = '/home/ida/Documents/GitHub/FEniCS/Phase_Field_Fracture/StressProfilePlaneStress/';
+% DataPath = '/Users/idaang/Documents/GitHub/FEniCS/Phase_Field_Fracture/StressProfile/';
 count = 1; 
 for i = 1:length(ellx)
     hsize(i) = Y/Ny(i);
@@ -32,12 +32,14 @@ for i = 1:length(ellx)
     NominalStress(i,:) = DataMatrix.NominalStress4; 
     
     figure(2)
-    subplot(2,2,count)     
-    title(SubPlotTitle{count})
+%     subplot(2,2,count)     
+%     title(SubPlotTitle{count})
     hold on 
     h2(i) = plot(Points(i,:), NominalStress(i,:), 'Color', colors{1,i}, 'LineWidth', 2.5);
     set(gca, 'YScale', 'log')
-    ylim([1E-4 1])
+    set(gca, 'YScale', 'linear')
+
+%     ylim([1E-5 1])
     
     if i == 4 || i == 8 || i == 12
         count = count + 1; 
@@ -53,7 +55,7 @@ leg = legend([h2(9), h2(10), h2(11), h2(12)], ["20: 0.16", "40: 0.32", "60: 0.48
 leg = legend([h2(13), h2(14), h2(15), h2(16)], ["20: 0.08", "40: 0.16", "60: 0.24", "80: 0.32"], 'Location', 'Best');
 title(leg, "Elements");
 title(leg, "$\kappa$", 'Interpreter', 'Latex');
-saveas(gcf, "StressProfile_Nx_v_Ny_v_k_10_hsize_v.fig")
+saveas(gcf, "StressProfile_PlaneStress_Nx_250_Ny_100_k_10.fig")
 
 %%
 xline(0, '--k')
