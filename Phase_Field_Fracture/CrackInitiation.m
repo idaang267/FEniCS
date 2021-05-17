8459f9e522bd0608f40d466cf05f7f0e3c298b24
ida <>
2021-05-17 08:47:16 -0400

    Result file

--- Phase_Field_Fracture/CrackInitiation.m
+++ Phase_Field_Fracture/CrackInitiation.m
@@ -44,6 +44,23 @@ plot(n_steps, Delta, '*')
 plot(x, y, '*')
 
 %%
+Delta = [0, 0.1, 2.58, 2.59, 2.6]'; 
+n_steps = [0, 1, 198 , 199, 200]'; 
+
+coefs = fit(n_steps, Delta, 'exp2')
+
+a = coefs.a;
+b = coefs.b;
+c = coefs.c;
+d = coefs.d;
+
+x = linspace(0, 200, 200);
+y = a*exp(b*x) + c*exp(d*x);
+
+hold on
+plot(n_steps, Delta, '*')
+plot(x, y, '*')
+%%
 Dis = TaylorHoodenergies.e03;
 ElaEnergy = TaylorHoodenergies.e1;
 DisEnergy = TaylorHoodenergies.e00;
